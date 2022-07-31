b0b0711d71c40078634cc02c36b704861be5fedf
abdullah_mamun <aamun22@gmail.com>
2022-07-31 10:10:24 -0700

    javascript function practice

---
+++ day-01/main.js
@@ -0,0 +1,81 @@
+/*+++++++++++++++++++++++++
+================================
++++++++++++++++++++++++++++*/
+
+// function main() {
+//     var goalsTeam1 = parseInt(prompt(), 10);
+//     var goalsTeam2 = parseInt(prompt(), 10);
+//     // function call
+//     finalResult(goalsTeam1, goalsTeam2)
+    
+// }
+// //complete the function
+// function finalResult(goalsTeam1,goalsTeam2) {
+//     if(goalsTeam1 >goalsTeam2){
+//         console.log( "Team 1 won");
+//     }else if(goalsTeam1 < goalsTeam2){
+//         console.log( "Team 2 won");
+//     }
+//     else if (goalsTeam1==goalsTeam2){
+//         console.log( "Draw");
+//     }
+    
+    
+// };
+// main();
+
+/*+++++++++++++++++++++++++
+================================
++++++++++++++++++++++++++++*/
+
+// function variableCalling(){
+//     var num1 = parseInt(prompt(),10);
+//     var num2 = parseInt(prompt(),10);
+//     var num3 = parseInt(prompt(),10);
+//     addition(num1,num2,num3);
+// }
+
+// function addition(num1,num2,num3){
+//     var sum = num1+num2+num3;
+//     console.log("Result =" +sum);
+// }
+// variableCalling();
+
+/*+++++++++++++++++++++++++
+You are given a program that takes 3 numbers as input.
+Complete the given function to calculate the average of those 3 numbers, 
+assign it to the given variable, and output it.
++++++++++++++++++++++++++++*/
+
+// function variableCalling(){
+//     var num1 = parseInt(prompt(),10);
+//     var num2 = parseInt(prompt(),10);
+//     var num3 = parseInt(prompt(),10);
+//     addition(num1,num2,num3);
+// }
+
+// function addition(num1,num2,num3){
+//     var average = (num1+num2+num3)/3;
+//     console.log("Result =" +average);
+// }
+// variableCalling();
+/*===================================================================================
+ * You are making a currency converter app.
+Create a function called convert, which takes two parameters: the amount to convert,
+ and the rate, and returns the resulting amount.
+The code to take the parameters as input and call the function is already present in the Playground.
+Create the function to make the code work.
+
+ ========================================================================================*/
+ 
+//  function callVariable(){
+//     var amount= parseFloat(prompt(),10)
+//     var rate= parseFloat(prompt());
+//     convert(amount,rate);
+//  }
+ 
+//  function convert(amount,rate){
+//     var result = amount*rate;
+//     console.log(result);
+//  }
+//  callVariable();
