23fbe20865639fdd7fda51601c144b299edf7804
abdullah_mamun <aamun22@gmail.com>
2022-08-01 05:28:57 -0700

    Learnig Function

---
+++ day-02/main.js
@@ -0,0 +1,14 @@
+/**
+ * 2. Write a JavaScript function that checks whether a passed string is palindrome or not?
+ */
+
+/***+++++++++++++++++++++++++++++
+ * Basic Function or Method: string.replace();
+ +++++++++++++++++++++++++++++++++*/
+
+ var btn =document.querySelector("button");
+ var text = btn.innerHTML;
+ var text = text.replace("Microsoft","Google");
+ var text = text.replace("Visit","Go");
+ console.log(text);
+
